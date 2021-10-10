dc267dab26defc20f1d95f3b0353f5ed381da6dd
Altin <alt-2020@outlook.com>
2021-10-10 16:32:55 -0400

    rem(createlist): console log

--- App/CreateShoppingList/CreateShoppingList.js
+++ App/CreateShoppingList/CreateShoppingList.js
@@ -95,7 +95,6 @@ export default function CreateShoppingList(props) {
 	];
 
 	const [items, setItems] = useState([...startingItems])
-	console.log(items)
 
 	const deleteItem = (name) => {
 		const i = items.findIndex((item) => item.name === name);
