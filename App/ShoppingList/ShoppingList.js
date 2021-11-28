b0fa2e6a9f647247d406a3fdac19f6ef1a825ebb
Alex Stiles - BLA <alexstiles@ballastlane.com>
2021-11-28 16:59:37 -0500

    manage list ui changes

--- App/ShoppingList/ShoppingList.js
+++ App/ShoppingList/ShoppingList.js
@@ -6,49 +6,100 @@
 import React from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
 
-export default function ShoppingList({ data, navigation, route }) {
-  
-  function isExpired(shelfLife, itemName) {
+export default function ShoppingList({ route }) {
+  function isExpired(shelfLife) {
+    if (!shelfLife) {
+      return <Text style={styles.maxWidth}>This item does not expire</Text>;
+    }
     var currentDate = new Date();
     var prevDate = new Date(route.params.dateCreated);
     var difference_in_time = currentDate.getTime() - prevDate.getTime();
     var difference_in_days = difference_in_time / (1000 * 3600 * 24);
+    let days_left = shelfLife - difference_in_days;
+    let hours_left = Math.floor((days_left % 1) * 24);
     if (difference_in_days > shelfLife) {
-      return <Text style={styles.expired}>{itemName}   Expired</Text>
+      return <Text style={styles.expired}>This item has expired!</Text>;
     }
-    return <Text style={styles.maxWidth}> {itemName}</Text>
+    return (
+      <Text style={styles.maxWidth}>
+        {" "}
+        {Math.floor(days_left)} day(s), {hours_left} hour(s) left before
+        expiration.
+      </Text>
+    );
+  }
+
+  function boolIsExpired(shelfLife) {
+    var currentDate = new Date();
+    var prevDate = new Date(route.params.dateCreated);
+    var difference_in_time = currentDate.getTime() - prevDate.getTime();
+    var difference_in_days = difference_in_time / (1000 * 3600 * 24);
+    if(difference_in_days > shelfLife) {
+      return true
+    } else {
+      return false
+    }
   }
 
   return (
     <View style={styles.shoppingListView}>
       <Text style={styles.listTitle}>{route.params.title}</Text>
-        {route.params.items.map((shoppingItem) => {
+      <Text style={styles.listsubheader}>Created on {route.params.dateCreated}</Text>
+      {route.params.items.map((shoppingItem) => {
         return (
           <View key={shoppingItem.name} style={styles.listRow}>
-            <View style={styles.flexbox}>
-              {isExpired(shoppingItem.shelfLife, shoppingItem.name)}
-              <View style={[styles.flexbox, styles.alignRight]}>
-                <Image
-                  source={require("./../../assets/images/trash.png")}
-                  style={styles.trashIcon}
-                />
-                <Image
-                  source={require("./../../assets/images/silverware.png")}
-                  style={styles.silverwareIcon}
-                />
-              </View>
-            </View>
             <View style={styles.hr}></View>
+            <View style={styles.flexbox}>
+              <Text style={{ fontWeight: "bold" }}>{shoppingItem.name}</Text>
+            </View>
+            <View style={{ marginTop: 20 }}>
+              {isExpired(shoppingItem.shelfLife, shoppingItem.name)}
+            </View>
+            <View style={[styles.flexbox, styles.wasteIcons]}>
+              <View style={styles.centerAlign}>
+                <View style={styles.imageAlign}>
+                  <Image
+                    source={require("./../../assets/images/trash.png")}
+                    style={styles.trashIcon}
+                  />
+                </View>
+                <Text>Thrown out / Expired</Text>
+              </View>
+              {!boolIsExpired(shoppingItem.shelfLife) && 
+              <View style={styles.centerAlign}>
+                <View style={styles.imageAlign}>
+                  <Image
+                    source={require("./../../assets/images/silverware.png")}
+                    style={styles.silverwareIcon}
+                  />
+                </View>
+                <Text>Consumed</Text>
+              </View>
+              }
+            </View>
           </View>
-        )
+        );
       })}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  imageAlign: {
+    display: "flex",
+    justifyContent: 'center',
+    height: 60,
+  },
   listRow: {
     padding: 0,
+  },
+  centerAlign: {
+    marginTop: 20,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "50%",
   },
   maxWidth: {
     maxWidth: 250,
@@ -81,10 +132,16 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   alignRight: {
-	  marginLeft: 'auto',
+    marginLeft: "auto",
   },
   listTitle: {
-    fontSize: 20,
-    textAlign: 'center'
-  }
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: "center",
+  },
+  listsubheader: {
+    fontSize: 18,
+    fontWeight: '400',
+    textAlign: "center",
+  },
 });
