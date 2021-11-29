f8178e85477d3bceee3b42e9cdd769c2ffb27459
David Sun <sundai@umich.edu>
2021-11-29 15:58:29 -0500

    Added logic for line chart (needs to be updated) and pie chart

--- App/HomePage/HomePage.js
+++ App/HomePage/HomePage.js
@@ -7,10 +7,13 @@
 //
 
 import React from "react"
+import { useState, useEffect } from "react";
+import { useIsFocused } from "@react-navigation/native";
 import { Image, StyleSheet, Text, View, Dimensions, ScrollView , StatusBar, } from "react-native"
 import {
 	LineChart, PieChart
 } from "react-native-chart-kit";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 // import PieChart from 'react-native-pie-chart';
 
 
@@ -18,153 +21,184 @@ import {
 
 
 
-  export default class HomePage extends React.Component {
+export default function HomePage() {
 
-	static navigationOptions = ({ navigation }) => {
+	// static navigationOptions = ({ navigation }) => {
 	
-		const { params = {} } = navigation.state
-		return {
-				header: null,
-				headerLeft: null,
-				headerRight: null,
-			}
-	}
+	// 	const { params = {} } = navigation.state
+	// 	return {
+	// 		header: null,
+	// 		headerLeft: null,
+	// 		headerRight: null,
+	// 	}
+	// }
 
-	constructor(props) {
-		super(props)
-		this.state = {
-			data1: 10,
-			data2: 20,
-			data3: 30,
-			data4: 40
-			};
-	}
+	// constructor(props) {
+	// 	super(props)
+	// 	this.state = {
+	// 		data1: 10,
+	// 		data2: 20,
+	// 		data3: 30,
+	// 		data4: 40
+	// 	};
+	// }
+	const isFocused = useIsFocused();
+	const [lineChartData, setLineChartData] = useState([0, 0, 0, 0, 0, 0]);
+	const [pieChartData, setPieChartData] = useState([{ name: "N/A", value: 0 }, { name: "N/A", value: 0 }, { name: "N/A", value: 0 }, { name: "N/A", value: 0 },])
 
-	componentDidMount() {
+
+	useEffect(() => {
+		try {
+			getWasteData().then((res) => {
+				let parsed = JSON.parse(res);
+				let numWasted = parsed.length
+				setLineChartData([numWasted, 0, 0, 0, 0, 0])
+				
+				const wastedItems = {};
+				parsed.forEach((obj) => {
+					const key = `${obj.name}`;
+					if (!wastedItems[key]) {
+					   wastedItems[key] = { ...obj, count: 0 };
+					};
+					wastedItems[key].count += 1;
+				 });
+				let counts = (Object.values(wastedItems)) // Displays count of each
+				
+				counts.sort((a, b) => (a.count <= b.count) ? 1 : -1)
+				
+				let sorted = [];
+				// Don't display pie chart data until there are atleast 4 wasted food items
+				for (let i = 0; i < 4; i++) {
+					sorted.push({name: counts[i].name.substr(0, counts[i].name.indexOf(',')), value: (counts[i].count / numWasted) * 100})
+				}
+				if (counts.length >= 4) {
+					setPieChartData(sorted);
+				}	
+			   });
+		  }
+		  catch (e) {
+			console.log(e)
+		  }
+	  }, [isFocused]);
 	
-	}
-
-	// Example line chart for home screen
-	 line = {
-		labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-		datasets: [
-			{
-				data: [20, 45, 28, 80, 99, 43],
-				strokeWidth: 2, // optional
-			},
-		],
+	getWasteData = async () => {
+		try {
+			return await AsyncStorage.getItem("my-waste");
+		} catch (e) {
+			return e;
+		}
 	};
 	
-
-	  render() {
-		  
-		// Example Pie Chart data
-		  const screenWidth = Dimensions.get("window").width;
-		  const pieData = [
+		//  Pie Chart data
+		const screenWidth = Dimensions.get("window").width;
+		const pieData = [
 			{
-			name: "Milk",
-			value: this.state.data1,
-			color: "#855CF8",
-			legendFontColor: "#181818",
-			legendFontSize: 15
+				name: pieChartData[0].name,
+				value: pieChartData[0].value,
+				color: "#855CF8",
+				legendFontColor: "#181818",
+				legendFontSize: 13
 			},
 			{
-			name: "Steak",
-			value: this.state.data2,
-			color: "#E289F2",
-			legendFontColor: "#181818",
-			legendFontSize: 15
+				name: pieChartData[1].name,
+				value: pieChartData[1].value,
+				color: "#E289F2",
+				legendFontColor: "#181818",
+				legendFontSize: 13
 			},
 			{
-			name: "Bananas",
-			value: this.state.data3,
-			color: "#503795",
-			legendFontColor: "#181818",
-			legendFontSize: 15
+				name: pieChartData[2].name,
+				value: pieChartData[2].value,
+				color: "#503795",
+				legendFontColor: "#181818",
+				legendFontSize: 13
 			},
 			{
-			name: "Butter",
-			value: this.state.data4,
-			color: "#B085FF",
-			legendFontColor: "#181818",
-			legendFontSize: 15
+				name: pieChartData[3].name,
+				value: pieChartData[3].value,
+				color: "#B085FF",
+				legendFontColor: "#181818",
+				legendFontSize: 13
 			}
-			];
-			const chartConfig = {
+		];
+		const chartConfig = {
 			backgroundGradientFrom: "#1E2923",
 			backgroundGradientFromOpacity: 0,
 			backgroundGradientTo: "#08130D",
 			backgroundGradientToOpacity: 0.5,
 			color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
 			strokeWidth: 2, // optional, default 3
-			barPercentage: 0.5
-		  };
-		  
-		  return (
-			  <View style={styles.container}>
-				  <Text style={styles.HomeText}>GreenHouse</Text>
-				  <Text style = {styles.foodWasteText}> Food Waste History </Text>
-				  <LineChart
-					  data={{
-						  labels: ["January", "February", "March", "April", "May", "June"],
-						  datasets: [
-							  {
-								  data: [
-									  Math.random() * 20,
-									  Math.random() * 20,
-									  Math.random() * 20,
-									  Math.random() * 20,
-									  Math.random() * 20,
-									  Math.random() * 20
-								  ]
-							  }
-						  ]
-					  }}
-					  width={Dimensions.get("window").width} // from react-native
-					  height={220}
-					  yAxisLabel=""
-					  yAxisSuffix="lb"
-					  yAxisInterval={1} // optional, defaults to 1
-					  chartConfig={{
-						  backgroundColor: "#55E085",
-						  backgroundGradientFrom: "#55E085",
-						  backgroundGradientTo: "green",
-						  decimalPlaces: 2, // optional, defaults to 2dp
-						  color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-						  labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-						  style: {
-							  borderRadius: 16
-						  },
-						  propsForDots: {
-							  r: "6",
-							  strokeWidth: "2",
-							  stroke: "#ffa726"
-						  }
-					  }}
-					  bezier
-					  style={{
-						  marginVertical: 8,
-						  borderRadius: 16
-					  }}
-				  />
-				  
-			<View style={{flex:1, justifyContent:'space-evenly', alignItems: 'center', }}>
-				<Text style={{ fontSize:30 }}>Food Waste by Item</Text>
-					<PieChart
-					data={pieData}
-					width={screenWidth}
-					height={220}
-					chartConfig={chartConfig}
-					accessor="value"
-					backgroundColor="transparent"
-					paddingLeft="20"
-					
-					/>
-			</View>	  
-		 </View>
-		  );
-	  }
+			barPercentage: 0.5,
+			
+	};
+	// Data for line chart
+	const data = {
+		labels: ["Nov", "Dec", "Jan", "Feb", "Mar", "Apr"],
+		datasets: [
+			{
+				data: lineChartData,
+			}
+		  ],
+		  legend: ["Items Wasted"] // optional
+		
 	}
+		
+		  
+		return (
+			<View style={styles.container}>
+				<Text style={styles.HomeText}>GreenHouse</Text>
+				<Text style={styles.foodWasteText}> Food Waste History </Text>
+				<LineChart
+					data={data}
+					width={Dimensions.get("window").width - 20} // from react-native
+					height={240}
+					yAxisLabel=""
+					yAxisSuffix=""
+					yAxisInterval={1} // optional, defaults to 1
+					fromZero="True"
+				
+					chartConfig={{
+						backgroundColor: "#55E085",
+						backgroundGradientFrom: "#55E085",
+						backgroundGradientTo: "green",
+						decimalPlaces: 2, // optional, defaults to 2dp
+						color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+						labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+						style: {
+							borderRadius: 16
+						},
+						propsForDots: {
+							r: "6",
+							strokeWidth: "2",
+							stroke: "#ffa726"
+						},
+						propsForLabels: {
+							fontSize: "14"
+						}
+					}}
+					bezier
+					style={{
+						marginVertical: 8,
+						borderRadius: 20
+					}}
+				/>
+				  
+				<View style={{ flex: 1, justifyContent: 'space-evenly', alignItems: 'center', }}>
+					<Text style={{ fontSize: 30 }}>Food Waste by Item</Text>
+					<PieChart
+						data={pieData}
+						width={screenWidth + 30}
+						height={220}
+						chartConfig={chartConfig}
+						accessor="value"
+						backgroundColor="transparent"
+						paddingLeft="10"
+					/>
+				</View>
+			</View>
+		);
+	}
+
 
 
 const styles = StyleSheet.create({
